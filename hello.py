4c808e66844854bf50a5717f7c7d4c3928896809
Anastasiiasyvak <asyvak@kse.org.ua>
2024-01-23 17:02:27 +0200

    first endpoint

--- hello.py
+++ hello.py
@@ -1,14 +1,30 @@
-from flask import Flask # клас Flask дозволить нам створити екземпляр веб-додатку
+from flask import Flask, request, jsonify
 
-app = Flask(__name__) # екземпляр і __name__ для того щоб flask коректно розпізнавав шляхи до файлів
+app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/metadata', methods=['POST'])
+def get_metadata():
+    if 'file' not in request.files or 'string' not in request.form:
+        return jsonify({'error': 'Missing file or search_string parameter'}), 400
 
-# у моєму випадку не виконується так як flask займається запуском серверу а не мій код 
+    file = request.files['file']
+    string = request.form['string'].lower()
+
+    if file.filename == '':
+        return jsonify({'error': 'Empty file provided'}), 400
+
+    content = file.read().decode('utf-8')
+
+    metadata = {
+        'length of whole text': len(content),
+        'amount of alphanumeric symbols': sum(i.isalnum() for i in content),
+        'number of occurrences of string': content.lower().count(string)
+    }
+
+    # isalnum +1 символ коли проходиться по файлу, якщо символ є буквою або цифрою
+
+    return jsonify(metadata)
+
+
 if __name__ == '__main__':
-    app.run()
-
-
-# flask run
+    app.run(debug=True)
